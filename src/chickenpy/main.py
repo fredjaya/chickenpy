4c3d39360f2a0eed54a3782eb23cdf241f4d092c
Fred Jaya <36436914+fredjaya@users.noreply.github.com>
2024-07-04 23:03:41 +1000

    Initial commit

---
+++ src/chickenpy/main.py
@@ -0,0 +1,15 @@
+from typing import Optional
+
+class WholeChicken:
+    
+    def butcher(self) -> dict[str, int]:
+        return {
+            "breast" : 2,
+            "thigh": 2,
+            "drumstick" : 2,
+            "wing" : 2,
+            "carcass" : 1
+        }
+
+    def roast(self):
+        return
